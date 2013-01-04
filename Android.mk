0d15abfc4319feaf4560dd12674753d7e8a95157
Iwo Banas <banas.iwo@gmail.com>
2013-01-04 23:12:52 +0000

    add logging

--- Android.mk
+++ Android.mk
@@ -8,5 +8,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := \
     screenrec.cpp \
 
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+
 include $(BUILD_EXECUTABLE)
 
--- screenrec.cpp
+++ screenrec.cpp
@@ -1,6 +1,11 @@
+#define LOG_NDEBUG 0
+#define LOG_TAG "screenrec"
+
 #include<stdio.h>
+#include<cutils/log.h>
 
 int main(int argc, char* argv[]) {
     printf("Screen Recorder\n");
+    ALOGV("TEST");
 }
 
