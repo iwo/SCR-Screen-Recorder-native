c11fb49482f7a5c584a554ea7515d132b799bd9a
Iwo Banas <banas.iwo@gmail.com>
2013-01-04 23:46:48 +0000

    OpenGL boilerplate

--- screenrec.cpp
+++ screenrec.cpp
@@ -4,8 +4,101 @@
 #include<stdio.h>
 #include<cutils/log.h>
 
+#include <media/mediarecorder.h>
+#include <gui/SurfaceTextureClient.h>
+
+static const char sVertexShader[] =
+    "attribute vec4 vPosition;\n"
+    "attribute vec2 texCoord; \n"
+    "varying vec2 tc; \n"
+    "void main() {\n"
+    "  tc = texCoord;\n"
+    "  gl_Position = mat4(0,1,0,0,  1,0,0,0, 0,0,-1,0,  0,0,0,1) * vPosition;\n"
+    "}\n";
+
+static const char sFragmentShader[] =
+    "precision mediump float;\n"
+    "uniform sampler2D textureSampler; \n"
+    "varying vec2 tc; \n"
+    "void main() {\n"
+    //"  gl_FragColor = vec4(0.0, 1.0, 0, 1.0);\n"
+    "  gl_FragColor.bgra = texture2D(textureSampler, tc); \n"
+    "}\n";
+
+static void checkGlError(const char* op) {
+    for (GLint error = glGetError(); error; error
+            = glGetError()) {
+        ALOGI("after %s() glError (0x%x)\n", op, error);
+    }
+}
+
+GLuint loadShader(GLenum shaderType, const char* pSource) {
+    GLuint shader = glCreateShader(shaderType);
+    if (shader) {
+        glShaderSource(shader, 1, &pSource, NULL);
+        glCompileShader(shader);
+        GLint compiled = 0;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (!compiled) {
+            GLint infoLen = 0;
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+            if (infoLen) {
+                char* buf = (char*) malloc(infoLen);
+                if (buf) {
+                    glGetShaderInfoLog(shader, infoLen, NULL, buf);
+                    ALOGE("Could not compile shader %d:\n%s\n",
+                            shaderType, buf);
+                    free(buf);
+                }
+                glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+    }
+    return shader;
+}
+
+GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
+    GLuint vertexShader = loadShader(GL_VERTEX_SHADER, pVertexSource);
+    if (!vertexShader) {
+        return 0;
+    }
+
+    GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, pFragmentSource);
+    if (!pixelShader) {
+        return 0;
+    }
+
+    GLuint program = glCreateProgram();
+    if (program) {
+        glAttachShader(program, vertexShader);
+        checkGlError("glAttachShader");
+        glAttachShader(program, pixelShader);
+        checkGlError("glAttachShader");
+        glLinkProgram(program);
+        GLint linkStatus = GL_FALSE;
+        glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+        if (linkStatus != GL_TRUE) {
+            GLint bufLength = 0;
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
+            if (bufLength) {
+                char* buf = (char*) malloc(bufLength);
+                if (buf) {
+                    glGetProgramInfoLog(program, bufLength, NULL, buf);
+                    ALOGE("Could not link program:\n%s\n", buf);
+                    free(buf);
+                }
+            }
+            glDeleteProgram(program);
+            program = 0;
+        }
+    }
+    return program;
+}
+
 int main(int argc, char* argv[]) {
     printf("Screen Recorder\n");
     ALOGV("TEST");
 }
+
 
