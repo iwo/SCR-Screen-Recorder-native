0b598f75425ae25d1706d31b77f587e452559d75
Iwo Banas <banas.iwo@gmail.com>
2013-01-12 12:26:26 +0000

    make test script portable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-adb -d push ../../../../out/target/product/galaxysmtd/system/bin/screenrec /data/local/tmp/screenrec
+adb -d push $OUT/system/bin/screenrec /data/local/tmp/screenrec
 adb -d shell su -c "/data/local/tmp/screenrec /sdcard/screenrec.mp4"
 adb pull /sdcard/screenrec.mp4  ../../../../..
 
